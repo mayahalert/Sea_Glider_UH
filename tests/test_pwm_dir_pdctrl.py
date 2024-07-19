d5a4a30eb3a047461469666a699c30dc2d3b94bb
mayahalert <mayahw@hawaii.edu>
2024-07-19 09:40:12 -1000

    Update test_pwm_dir_pdctrl.py

--- tests/test_pwm_dir_pdctrl.py
+++ tests/test_pwm_dir_pdctrl.py
@@ -3,6 +3,10 @@ from control.PD_controller import PDController
 from hardware.adc import read_adc
 from hardware.pwm import PWMController
 from hardware.gpio import GPIOControl
+from utils.logger import testData # Import custom testData class
+
+#create instance of testData
+logger=testData(filename='test_pwm_dir_pdctrl_testData.txt')
 
 pwm = PWMController(PWM_CHIP_BASE, PWM_PERIOD_NS)
 
@@ -17,6 +21,9 @@ while pos > 350:
     pos = read_adc(PITCH_POT_PATH)
     output = controller.compute(pos)
     print(output)
+    #Log Data
+    testData.log("position",pos)
+    testData.log("output",output)
     if output < 0:
         gpio.set_value(351, 0)
         pwm.set_pwm_dc(M4_PATH, abs(output))
@@ -27,4 +34,11 @@ while pos > 350:
     #print(pos)
     
 pwm.set_pwm_dc(M4_PATH, 0)
+
+#Output testData data
+print(".txt file generated")
+
+#print("test data: ")
+#for label,value in logger.subscribe():
+#    print(f"{label}: {value}")
         
