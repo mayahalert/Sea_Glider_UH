d881eab21ae9b1052b0f4c2b1c4320daa21b2d95
mayahalert <mayahw@hawaii.edu>
2024-07-22 10:47:09 -1000

    Update test_pwm_dir_pdctrl.py
    Option to use test data filename in log file

--- tests/test_pwm_dir_pdctrl.py
+++ tests/test_pwm_dir_pdctrl.py
@@ -5,25 +5,26 @@ from hardware.pwm import PWMController
 from hardware.gpio import GPIOControl
 from utils.logger import testData # Import custom testData class
 
-#create instance of testData
-logger=testData(filename='test_pwm_dir_pdctrl_testData.txt')
+#create instance of testData with filename based on current script name and time
+logger = testData()
+# or enter it manually
+#logger=testData(filename='test_pwm_dir_pdctrl_testData.txt')
 
 pwm = PWMController(PWM_CHIP_BASE, PWM_PERIOD_NS)
-
 gpio = GPIOControl()
 gpio.setup_pin(351, direction='out', default_value=0)
-
 controller = PDController(Kp=20, Kd=3, setpoint=350)
-
 pos = read_adc(PITCH_POT_PATH)
 
 while pos > 350:
     pos = read_adc(PITCH_POT_PATH)
+    loggger.log("postiion", pos)
+    #compute output from PD controller
     output = controller.compute(pos)
+    #log PD Controller output
+    logger.log("output",output)
     print(output)
-    #Log Data
-    testData.log("position",pos)
-    testData.log("output",output)
+
     if output < 0:
         gpio.set_value(351, 0)
         pwm.set_pwm_dc(M4_PATH, abs(output))
@@ -36,9 +37,9 @@ while pos > 350:
 pwm.set_pwm_dc(M4_PATH, 0)
 
 #Output testData data
-print(".txt file generated")
+print("file generated" logger.filename,)
 
-#print("test data: ")
-#for label,value in logger.subscribe():
-#    print(f"{label}: {value}")
+# Optionally, print logged data
+print("Logged data:")
+print(logger_auto.subscribe())
         
